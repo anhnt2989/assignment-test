f4ac5b5b6c6f8ae559c4c90ec3142c167509275d
Chubby-Dev <anhnt2989@gmail.com>
2021-04-25 19:08:44 +0700

    [Weather Widget] - Edited: subscribe temp & icon

--- src/components/LocationInput/Wrapper.ts
+++ src/components/LocationInput/Wrapper.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 import Input from 'antd/lib/input'
 
-const StyledInput = styled(Input)``
+const StyledInput = styled(Input)`
+  background: #FFFFFF;
+  border: 1px solid rgba(150, 150, 150, 0.3);
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+`
 
 export default StyledInput
